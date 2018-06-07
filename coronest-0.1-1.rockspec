556f8bcb11f813f3c8f88d9d5e5d672e5362136a
LunaCI <lunaci@luadist.org>
2018-06-07 23:59:33 +0000

    Update to version 0.1-1

---
+++ coronest-0.1-1.rockspec
@@ -0,0 +1,44 @@
+-- This file was automatically generated for the LuaDist project.
+
+package = "coronest"
+version = "0.1-1"
+
+-- LuaDist source
+source = {
+  tag = "0.1-1",
+  url = "git://github.com/LuaDist-testing/coronest.git"
+}
+-- Original source
+-- source = {
+--   url = "git://github.com/saucisson/lua-coronest",
+--   tag = "0.1",
+-- }
+
+description = {
+  summary     = "Coroutines that allow nesting",
+  detailed    = [[
+Coroutines are a powerful notion to suspend and resume execution of
+lightweight threads. They can be used for instance for scheduling,
+or for iteration.
+
+When mixing these two uses, a problem arises: there is no way to specify
+which `coroutine.resume` should intercept a `coroutine.yield`.
+
+This module provides a thin wrapper around the standard `coroutine`
+module to allow the definition of complex behaviors with nested
+coroutines.
+  ]],
+  license     = "MIT/X11",
+  maintainer  = "Alban Linard <alban@linard.fr>",
+}
+
+dependencies = {
+  "lua >= 5.1",
+}
+
+build = {
+  type    = "builtin",
+  modules = {
+    ["coroutine.make"] = "src/coroutine/make.lua",
+  },
+}
---
+++ src/coroutine/make.lua
@@ -0,0 +1,76 @@
+-- Coroutines that allow nesting
+-- =============================
+--
+-- Coroutines are a powerful notion to suspend and resume execution of
+-- lightweight threads. It can be used for instance for scheduling in
+-- [copas](http://keplerproject.github.io/copas/), or for
+-- [iteration](http://www.lua.org/pil/9.3.html).
+--
+-- When mixing these two uses, a problem arises: there is no way to specify
+-- which `coroutine.resume` should intercept a `coroutine.yield`.
+-- See: http://stackoverflow.com/questions/27123966/lua-nested-coroutines
+--
+-- This module provides a thin wrapper around the standard `coroutine`
+-- module to allow the definition of complex behaviors with nested
+-- coroutines.
+
+local select      = select
+local create      = coroutine.create
+local isyieldable = coroutine.isyieldable
+local resume      = coroutine.resume
+local running     = coroutine.running
+local status      = coroutine.status
+local wrap        = coroutine.wrap
+local yield       = coroutine.yield
+
+return function (tag)
+  local coroutine = {
+    isyieldable = isyieldable,
+    running     = running,
+    status      = status,
+  }
+  tag = tag or {}
+
+  local function for_wrap (co, ...)
+    if tag == ... then
+      return select (2, ...)
+    else
+      return for_wrap (co, co (yield (...)))
+    end
+  end
+
+  local function for_resume (co, status, ...)
+    if not status then
+      return status, ...
+    elseif tag == ... then
+      return status, select (2, ...)
+    else
+      return for_resume (co, resume (co, yield (...)))
+    end
+  end
+
+  function coroutine.create (f)
+    return create (function (...)
+      return tag, f (...)
+    end)
+  end
+
+  function coroutine.resume (co, ...)
+    return for_resume (co, resume (co, ...))
+  end
+
+  function coroutine.wrap (f)
+    local co = wrap (function (...)
+      return tag, f (...)
+    end)
+    return function (...)
+      return for_wrap (co, co (...))
+    end
+  end
+
+  function coroutine.yield (...)
+    return yield (tag, ...)
+  end
+
+  return coroutine
+end
